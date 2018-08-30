0fa0d2fcb08c9f8651c771ce513267fe826f4aa4
sychovSaveliy <sychov.saveliy@gmail.com>
2018-08-30 21:14:46 +0300

    L_5 upd

--- lections/lection_5_arrays/materials/main.js
+++ lections/lection_5_arrays/materials/main.js
@@ -5,8 +5,8 @@ console.log('Lection_5');
 // arr = [1, 7, 34,83,6,35 ];
 
 
-n = 10;
-m = 10;
+n = 21;
+m = 21;
 
 // create
 arr = new Array(n);
@@ -15,30 +15,46 @@ for( i = 0; i < arr.length; i++ ){
 }
 
 // fill and output
-console.log(arr, 'END');
+// console.log(arr, 'END');
 
-for(i = 0; i < arr.length; i++){
-    // arr[i] == array
-    // console.log(arr[i])
-    for(j = 0; j < arr[i].length; j++) {
-        arr[i][j] = Math.floor(Math.random()*90 + 10);
-        document.write(arr[i][j] + ' ');
-    }
+// for(i = 0; i < arr.length; i++){
+//     // arr[i] == array
+//     // console.log(arr[i])
+//     for(j = 0; j < arr[i].length; j++) {
+//         arr[i][j] = Math.floor(Math.random()*90 + 10);
+//         document.write(i+''+j + ' ');
+//     }
 
-    document.write('<br />');
-}
+//     document.write('<br />');
+// }
 
-console.log(arr);
+// console.log(arr);
 
 // --------------------------------
+
+n = arr.length;
+k = Math.ceil(n / 2);
+console.log(k)
 document.write('<hr />');
 for(i = 0; i < arr.length; i++){
     for(j = 0; j < arr[i].length; j++) {
-        if (arr[i][j] < 50) {
-            arr[i][j] = '00';
-        }
-        document.write(arr[i][j] + ' ');
+        // if (i + j <= n-1 && j >= k && i >= k/2) {
+        //     document.write('1' + ' ');
+        // } else 
+        //     document.write('0' + ' ');
+
+        // if (i >= j && i+j>= n - 1) {
+        //     document.write('1' + ' ');
+        // } else 
+        //     document.write('0' + ' ');
+
+        if ((i + j <= n - 1 && i >= k/2 && i <= k + k/2 && j <= k) || (i >= k + k/2 && (j >= k + k/2)) )  {
+            document.write('1' + ' ');
+        } else 
+            document.write('0' + ' ');
     }
     document.write('<br />');
 }
+
+// --------------------------------
 
