1eb84cef8ed071704398e0c1090b283b052c1979
savelii.sychov <savelii.sychov@myheritage.com>
2018-09-13 19:36:32 +0300

    L_7

---
+++ lections/lection_7_functions-practice/materials/main.js
@@ -0,0 +1,69 @@
+console.log('Lection_7');
+
+function f(a, b) {
+    // console.log( arguments ); // -> [2, 6]
+    
+    // for(var i = 0; i < arguments.length; i++){
+    //     console.log(arguments[i]);
+    // }
+
+    return a + b;
+}
+
+var res = f(2, 6, 8, {}, 5, false,65);
+// console.log(res);
+
+// -------------------------------------------
+
+function func(a, b, callback) { // b = sum
+    // console.log( b == sum);
+    // console.log(callback);
+    // var res = callback(a, 300);
+    // console.log(res);
+    // return a*a;
+    var p = 100;
+    return callback(a, b);
+}
+
+
+
+
+// res = func(10);
+// console.log( typeof sum );
+// func(10, true);
+var p = 200;
+// console.log( func(10, 23, sum), func(10, 23,  div) );
+
+
+function sum(x, y){
+    return x + y + p;
+}
+
+function div(x, y){
+    return x / y + p;
+}
+
+
+// -------------------------------------------
+
+
+function f0() {
+    var z = 0;
+    f1(80);
+
+    function f1(a) {
+        var x = 200;
+    
+        f2();
+    
+    
+        function f2() {
+            console.log(x, y, a, z);
+            // debugger;
+        }
+    }
+}
+
+y = 12;
+
+f0();
