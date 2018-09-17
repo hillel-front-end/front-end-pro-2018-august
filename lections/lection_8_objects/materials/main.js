89d091656bf3f37709184eb31192b27fbbb1db07
lin4pin <skorkh.edu@gmail.com>
2018-09-17 21:23:22 +0300

    L_8_upd

--- lections/lection_8_objects/materials/main.js
+++ lections/lection_8_objects/materials/main.js
@@ -22,6 +22,11 @@ obj = {
     }
 }
 
+// console.log(obj);
+// for(key in obj) {
+//     console.log( key, obj[key] );
+//  }
+
 key = 'city'
 
 // console.log( obj[key] );
@@ -45,11 +50,11 @@ key = 'city'
 //     }
 // }
 
-// data.sum(2, 3);
+// // data.sum(2, 3);
 
-// ------------------
+// // ------------------
 
-// перебор
+// // перебор
 // console.log(obj);
 // for(key in obj) {
 //     console.log( key, obj[key] );
@@ -57,29 +62,27 @@ key = 'city'
 
 // ----------------------------------------------
 
-function sumSource(a, b){
-    // this -> context
-    console.log(this);
-    return a + b + this.x;
-}
+// function sumSource(a, b){
+//     // this -> context
+//     console.log(this);
+//     return a + b + this.x;
+// }
 
 
+// var data = {
+//     x: 10,
+//     y: 20,
+//     sum: sumSource
+// }
 
+// var data2 = {
+//     x: 20,
+//     sum: sumSource
+// }
 
-var data = {
-    x: 10,
-    y: 20,
-    sum: sumSource
-}
-
-var data2 = {
-    x: 20,
-    sum: sumSource
-}
-
-sumSource(2, 8);
-data.sum(1, 2);
-console.log(sumSource === data.sum)
+// sumSource(2, 8);
+// data.sum(1, 2);
+// console.log(sumSource === data.sum)
 
 // z = 200;
 // console.log(window.z);
@@ -88,3 +91,53 @@ console.log(sumSource === data.sum)
 // window.alert();
 // document.write();
 // window.prompt();
+
+// ----------------------------
+
+
+function mySplice(){
+    if (arguments.length < 2) {
+        return;
+    } 
+
+    var pos = arguments[0],
+        count = arguments[1],
+        res = [],
+        resLength = this.length - count + arguments.length - 2,
+        insetArr = [];
+
+    for(var i = 1; i <= count; i++) {
+        res[res.length] = this[pos + i - 1];
+    }
+
+    // insert
+    for(var i = pos + count, j = 0; i < this.length; i++, j++) {
+        this[pos + j] = this[i];
+    }
+
+    for(var i = 2; i < arguments.length; i++) {
+        insetArr[insetArr.length] = arguments[i];
+    }
+
+    for(var i = insetArr.length-1; i >= 0; i--) {
+        var item = insetArr[i];
+
+        this.length++;
+
+        for(var j = this.length - 1; j > pos; j--) {
+            this[j] = this[j - 1];
+        }
+        this[pos] = item;
+    }
+
+    this.length -= count;
+
+    return res;
+}
+
+list = [2,3,42,4,25,213,1,2];
+list.mySplice = mySplice;
+
+var resp = list.mySplice(2, 4, 'first', 'second', 'third');
+console.log(resp);
+console.log(list);
