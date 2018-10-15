28b51fe4c824321f22f27136d545d26da13a0a18
lin4pin <skorkh.edu@gmail.com>
2018-10-15 19:16:09 +0300

    L_13 upd

--- lections/lection_13_BOM_DOM/materials/main.js
+++ lections/lection_13_BOM_DOM/materials/main.js
@@ -41,12 +41,13 @@ window.onload = function(){
     // blocks.innerHTML += 'selected';
     // console.log(blocks);
 
+
     
     // class
 
     // --------------------------------------------
 
-    block = document.querySelector('.block');
+    // block = document.querySelector('.block');
 
     // block.className += ' selected'
     // block.classList.add('selected', 'fofo', 'fofo1');
@@ -55,5 +56,29 @@ window.onload = function(){
 //    var isSelected = block.classList.contains('selected');
 //    console.log(isSelected);
     // block.classList.toggle('selected');   
+
+
+     // ---------------------------------------
+
+
+     var elms = document.querySelectorAll('.block');
+
+     this.setInterval(function(){
+        for(i = 0; i < elms.length; i++){
+            elms[i].style.width = getRand(10, 300) + "px";
+            elms[i].style.left = getRand(0, window.innerWidth - elms[i].clientWidth) + 'px';
+            elms[i].style.top = getRand(0, window.innerHeight - elms[i].clientHeight) + 'px';
+            elms[i].style.backgroundColor = 'rgb('+ getRand(0, 256) +', ' + getRand(0, 256) +', ' + getRand(0, 256) + ')';
+        }
+     }, 500);
+     
+
+     console.log(elms);
+
 }
 
+
+
+function getRand(min, max){
+    return  Math.floor(Math.random()*(max - min) + min);
+ }
