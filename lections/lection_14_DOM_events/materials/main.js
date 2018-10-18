93ab3a91d3d3fee2f3ea310347e62a7d3073333e
savelii.sychov <savelii.sychov@myheritage.com>
2018-10-18 21:06:24 +0300

    L_14 upd

--- lections/lection_14_DOM_events/materials/main.js
+++ lections/lection_14_DOM_events/materials/main.js
@@ -35,13 +35,13 @@ window.onload = function(){
     // events
 
 
-    block.addEventListener('click', function(event){
-        console.log(event.type, event);
-    });
+    // block.addEventListener('click', function(event){
+    //     console.log(event.type, event);
+    // });
 
-    block.addEventListener('dblclick', function(event){
-        console.log(event.type, event);
-    });
+    // block.addEventListener('dblclick', function(event){
+    //     console.log(event.type, event);
+    // });
 
     // block.addEventListener('mouseover', function(event){
     //     console.log(event.type, event);
@@ -57,11 +57,36 @@ window.onload = function(){
 
     block.addEventListener('mousedown', function(event){
         console.log(event.type, event);
+
+        x0 = event.target.offsetLeft;
+        y0 = event.target.offsetTop;
+
+        mx = event.clientX;
+        my = event.clientY;
+
+        document.addEventListener('mouseup', function(){
+            document.removeEventListener('mousemove', MoveElem);
+        });
+
+        document.addEventListener('mousemove', MoveElem);
+
+        function MoveElem(ev){
+            mx2 = ev.clientX;
+            my2 = ev.clientY;
+            // console.log(mx2, my2)
+
+            diffx = mx2 - mx;
+            diffy = my2 - my;
+
+            event.target.style.left = x0 + diffx + 'px';
+            event.target.style.top = y0 + diffy + 'px';
+
+        }
     });
 
-    block.addEventListener('mouseup', function(event){
-        console.log(event.type, event);
-    });
+    // block.addEventListener('mouseup', function(event){
+    //     console.log(event.type, event);
+    // });
 
 
     
